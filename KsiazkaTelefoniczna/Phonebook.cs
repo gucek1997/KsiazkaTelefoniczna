8ca006e9b2ee21425c1a33bd7211f1af45887edb
Your Name <you@example.com>
2023-02-01 17:07:48 +0100

    KsiazkaTelefoniczna

---
+++ KsiazkaTelefoniczna/Phonebook.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsiazkaTelefoniczna
+{
+    class Phonebook
+    {
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+
+
+
+
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+
+        
+        private void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact: {contact.Name},{contact.Number}");
+
+        }
+
+
+
+
+
+
+        public void DisplayContact(string number)
+        {
+            var contact =Contacts.FirstOrDefault(c => c.Number == number);  
+            if(contact == null)
+            {
+                Console.WriteLine("Kontakt nieznaleziony");
+
+            }
+            else
+            {
+                DisplayContactDetails(contact); 
+            
+            }
+        }
+
+
+        public void DisplayAllContacts()
+        {
+
+            foreach (var contact in Contacts)
+            {
+                DisplayContactDetails(contact);
+
+            }
+
+        }
+
+
+        public void DisplayMatchingContacts(string searchPharse)
+        {
+
+            var matchingContacts = Contacts.Where(c => c.Name.Contains(searchPharse)).ToList();
+            foreach (var contact in matchingContacts)
+            {
+                DisplayContactDetails(contact);
+
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ KsiazkaTelefoniczna/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace KsiazkaTelefoniczna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Witamy w książce telefonicznej");
+
+            Console.WriteLine("1. Dodaj Kontakt");
+            Console.WriteLine("2.Wyświetl kontakt numer");
+            Console.WriteLine("3.Wyświetl wszystkie kontatky");
+            Console.WriteLine("4.Znajdź kontakt");
+            Console.WriteLine("X żeby zamknąć");
+
+            var userInput = Console.ReadLine();
+            var phoneBook = new Phonebook();
+
+            while(true)
+            {
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Wpisz numer");
+                        var numer = Console.ReadLine();
+                        Console.WriteLine("Wpisz imie");
+                        var name = Console.ReadLine();
+
+                        var newContact = new Contact(name, numer);
+
+                        phoneBook.AddContact(newContact);
+
+                        break;
+                    case "2":
+                        Console.WriteLine("Wpisz numer");
+                        var numerToSearch = Console.ReadLine();
+
+                        phoneBook.DisplayContact(numerToSearch);
+
+                        break;
+                    case "3":
+                        phoneBook.DisplayAllContacts();
+                        break;
+                    case "4":
+                        Console.WriteLine("Wyszukaj nazwę: ");
+                        var searchPhrase = Console.ReadLine();
+                        phoneBook.DisplayMatchingContacts(searchPhrase);
+                        break;
+                    case "x":
+                        return;
+                    
+                    default:
+                        Console.WriteLine("Niepoprawna operacja");
+                        break;
+                }
+                Console.WriteLine("Wybierz operacje: ");
+                userInput = Console.ReadLine();
+
+
+
+            }
+
+
+
+
+
+
+        
+        }
+    }
+}
